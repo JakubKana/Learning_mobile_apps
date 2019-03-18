0ccfb979fb92f22a03f64fb71af37f770b4167f9
Jakub Káňa <jakubkana@gmail.com>
2019-03-19 00:01:49 +0100

    TextInput, Stylesheet

--- RN_The_practical_guide/rnPlay/App.js
+++ RN_The_practical_guide/rnPlay/App.js
@@ -7,24 +7,42 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, TextInput, View } from "react-native";
+import replacePathSepForGlob from "jest-util/build/replacePathSepForGlob";
+import { black } from "ansi-colors";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    placeName: '',
+  }
+
+  placeNameChangedHandler = val => {
+    
+    this.setState({
+      placeName: val
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Awesome</Text>
+        <TextInput 
+        maxLength={40} 
+        editable 
+        style={{width:200, borderColor:"black", borderWidth:1}}
+        onChangeText={this.placeNameChangedHandler}
+        value={this.state.placeName}
+        />
       </View>
     );
   }
@@ -33,18 +51,11 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    padding: 26,
+    backgroundColor:"#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    
+
+  }
 });
