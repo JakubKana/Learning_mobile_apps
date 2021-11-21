75e075ee818f36b0bec6c1159feb0e2a146f3e0f
Mousehunter <baronsarhar@gmail.com>
2021-11-21 17:05:19 +0100

    Local notification

--- rn_project_push_notifications/App.js
+++ rn_project_push_notifications/App.js
@@ -1,33 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Button, View } from 'react-native';
-import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+    };
+  },
+});
 
 export default function App() {
   useEffect(() => {
     Permissions.getAsync(Permissions.NOTIFICATIONS)
       .then((statusObj) => {
-        if (statusObj.status !== 'granted') {
+        if (statusObj.status !== "granted") {
           return Permissions.askAsync(Permissions.NOTIFICATIONS);
         }
         return statusObj;
       })
       .then((statusObj) => {
-        if (statusObj.status !== 'granted') {
+        if (statusObj.status !== "granted") {
           return;
         }
       });
   }, []);
 
+  useEffect(() => {
+    const bgSubscription =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("Response", response);
+      });
+
+    const feSubscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("Object", notification);
+      }
+    );
+    return () => {
+      feSubscription.remove();
+      beSubscription.remove();
+    };
+  }, []);
+
   const triggerNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({
       content: {
-        title: 'My first local notification',
-        body: 'This is the first local notification we are sending!',
+        title: "My first local notification",
+        body: "This is the first local notification we are sending!",
+        data: { datas: { test: "data" } },
       },
       trigger: {
-        seconds: 10,
+        seconds: 5,
       },
     });
   };
@@ -46,8 +73,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
